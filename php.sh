96d7022ac063ae8f204a3b67ef7879e336c711b2
Ben Norcombe <b.norcombe@gmail.com>
2017-08-07 14:04:40 +0100

    Update cli image so it can be executed by host. Add php.sh for ease of use

---
+++ php.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+tty=
+    tty -s && tty=--tty
+    docker run \
+        $tty \
+        --interactive \
+        --rm \
+        --user $(id -u):$(id -g) \
+        --volume /etc/passwd:/etc/passwd:ro \
+        --volume /etc/group:/etc/group:ro \
+        --volume $(pwd):/app \
+        phpenv/cli "$@"
